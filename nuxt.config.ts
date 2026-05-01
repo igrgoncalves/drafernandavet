d73f2964742886bdb3da5b2546f9799c1021aabc
Igor Gonçalves <igorb32@hotmail.com>
2026-04-30 21:57:20 -0300

    config: set baseURL for github pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,6 +25,7 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
 
   app: {
+    baseURL: '/drafernandavet/',
     head: {
       title: 'Dra. Fernanda Moreno - Medicina Veterinária Integrativa',
       meta: [
